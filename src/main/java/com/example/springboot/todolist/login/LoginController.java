f9c4bb3288061b626c557abd70f68265566d321d
gargmanya1310 <gargmanya1310@gmail.com>
2025-02-06 15:38:22 +0530

    Learn the working of loggers and their use at different levels

--- src/main/java/com/example/springboot/todolist/login/LoginController.java
+++ src/main/java/com/example/springboot/todolist/login/LoginController.java
@@ -1,5 +1,7 @@
 package com.example.springboot.todolist.login;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -8,10 +10,14 @@ import org.springframework.web.bind.annotation.RequestParam;
 @Controller
 public class LoginController
 {
+    private Logger logger = LoggerFactory.getLogger(getClass());
     @RequestMapping("login")
     public String gotoLoginPage(@RequestParam String name , ModelMap model)
     {
         model.put("name", name);
+        logger.debug("Request Param is {}", name);
+        logger.info("I want it printed at info level");
+        logger.warn("I want it printed at warn level");
         System.out.println("Request Param is " + name);        //Not for production
 
         return "login";
