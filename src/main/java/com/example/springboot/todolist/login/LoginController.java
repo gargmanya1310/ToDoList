7a3537aabc7be36bff8fc1a7f6c166a875b0b0f8
gargmanya1310 <gargmanya1310@gmail.com>
2025-02-06 15:16:20 +0530

    Test login through requestparam

--- src/main/java/com/example/springboot/todolist/login/LoginController.java
+++ src/main/java/com/example/springboot/todolist/login/LoginController.java
@@ -1,14 +1,19 @@
 package com.example.springboot.todolist.login;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class LoginController
 {
     @RequestMapping("login")
-    public String gotoLoginPage()
+    public String gotoLoginPage(@RequestParam String name , ModelMap model)
     {
+        model.put("name", name);
+        System.out.println("Request Param is " + name);        //Not for production
+
         return "login";
     }
 }
--- src/main/resources/META-INF/resources/WEB-INF/jsp/login.jsp
+++ src/main/resources/META-INF/resources/WEB-INF/jsp/login.jsp
@@ -3,5 +3,6 @@
     <title>login page </title>
 </head>
 <body>
+welcome ${name}
 Hello from the login page</body>
 </html>
