dc808adc66b1733b78f0d8920c9915f9b1b20418
gargmanya1310 <gargmanya1310@gmail.com>
2025-02-07 18:29:42 +0530

    Show login form details back on redirection

--- src/main/java/com/example/springboot/todolist/login/LoginController.java
+++ src/main/java/com/example/springboot/todolist/login/LoginController.java
@@ -1,25 +1,25 @@
 package com.example.springboot.todolist.login;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class LoginController
 {
-    private Logger logger = LoggerFactory.getLogger(getClass());
-    @RequestMapping("login")
-    public String gotoLoginPage(@RequestParam String name , ModelMap model)
+    @RequestMapping(value="login", method= RequestMethod.GET)
+    public String gotoLoginPage()
     {
-        model.put("name", name);
-        logger.debug("Request Param is {}", name);
-        logger.info("I want it printed at info level");
-        logger.warn("I want it printed at warn level");
-        System.out.println("Request Param is " + name);        //Not for production
-
         return "login";
+    }
+    @RequestMapping(value="login", method= RequestMethod.POST)
+    public String gotoWelcomePage(@RequestParam String name, @RequestParam String password, ModelMap model)
+    {
+
+        model.put("name", name);
+        model.put("password", password);
+        return "welcome";
     }
 }
---
+++ src/main/resources/META-INF/resources/WEB-INF/jsp/welcome.jsp
@@ -0,0 +1,12 @@
+<html>
+    <head>
+        <title>Welcome page </title>
+    </head>
+    <body>
+
+    <div> Welcome to welcome page!</div>
+    <div> Your name: ${name}  </div>
+    <div> Your password : ${password} </div>
+
+    </body>
+</html>
