e7e10f0dd807b79603b64161498a27e5c90162f3
gargmanya1310 <gargmanya1310@gmail.com>
2025-02-14 18:11:57 +0530

    Add new to , create a view

--- src/main/java/com/example/springboot/todolist/todo/TodoController.java
+++ src/main/java/com/example/springboot/todolist/todo/TodoController.java
@@ -2,6 +2,7 @@ package com.example.springboot.todolist.todo;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.SessionAttributes;
 
 import java.util.List;
@@ -23,5 +24,19 @@ public class TodoController
         model.addAttribute("todos" , todos);
         return "listTodos";
     }
+@RequestMapping(value="add-todo", method= RequestMethod.GET)
+    public String showNewTodoPage( )
+    {
+        return "todo";
+    }
+
+@RequestMapping(value="add-todo", method= RequestMethod.POST)
+    public String addNewTodoPage( )
+    {
+
+        return "redirect:list-todos";
+    }
+
+
 
 }
