aca28dca1acd62495edabbc82dc75d01617f4a0e
gargmanya1310 <gargmanya1310@gmail.com>
2025-02-20 12:16:47 +0530

    Remove unwanted login code to prepare for Spring Security

---
+++ src/main/java/com/example/springboot/todolist/login/WelcomeController.java
@@ -0,0 +1,19 @@
+package com.example.springboot.todolist.login;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.SessionAttributes;
+
+@Controller
+@SessionAttributes("nameTest")
+public class WelcomeController
+{
+    @RequestMapping(value="/", method= RequestMethod.GET)
+    public String gotoWelcomePage(ModelMap model)
+    {
+        model.put("name", "Manya Garg");
+        return "welcome";
+    }
+}
