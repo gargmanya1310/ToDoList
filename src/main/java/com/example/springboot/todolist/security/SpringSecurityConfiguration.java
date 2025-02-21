1fe480c8b44f0d5008d782d094baf620317cb838
gargmanya1310 <gargmanya1310@gmail.com>
2025-02-21 14:54:37 +0530

    Configure Spring Security to Get H2 console Working

--- src/main/java/com/example/springboot/todolist/security/SpringSecurityConfiguration.java
+++ src/main/java/com/example/springboot/todolist/security/SpringSecurityConfiguration.java
@@ -2,13 +2,17 @@ package com.example.springboot.todolist.security;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
 
 import java.util.function.Function;
+
+import static org.springframework.security.config.Customizer.withDefaults;
 
 @Configuration
 public class SpringSecurityConfiguration
@@ -41,5 +45,24 @@ public class SpringSecurityConfiguration
     public PasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
     }
+
+    //All URLs are protected
+    //A login form is shown for unauthorized requests
+    //CSRF disable
+    //Frames
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+
+        http.authorizeHttpRequests(
+                auth -> auth.anyRequest().authenticated());
+        http.formLogin(withDefaults());
+
+        http.csrf(csrf -> csrf.disable());
+        http.headers(headers -> headers.frameOptions(frameOptionsConfig-> frameOptionsConfig.disable()));
+
+        return http.build();
+    }
+
 
 }
